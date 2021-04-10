db32c916c1172772ccc05ef3ac2c845708783ae0
曹家铭 <caojiaming@ipsec-172-16-72-202.vpn.gatech.edu>
2021-04-10 16:20:07 +0800

    debug makefile and includes

--- chap4/parse.c
+++ chap4/parse.c
@@ -3,11 +3,14 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "util.h"
 #include "symbol.h"
 #include "absyn.h"
 #include "errormsg.h"
 #include "parse.h"
+#include "prabsyn.h"
 
 extern int yyparse(void);
 extern A_exp absyn_root;
@@ -19,4 +22,17 @@ A_exp parse(string fname)
  if (yyparse() == 0) /* parsing worked */
    return absyn_root;
  else return NULL;
+}
+
+int main(int argc, char **argv) {
+ if (argc!=2) {fprintf(stderr,"usage: a.out filename\n"); exit(1);}
+ A_exp root = parse(argv[1]);
+
+ char *out_file_name = malloc(100);
+ strcpy(out_file_name, argv[1]);
+ strcat(out_file_name, "_parse.txt");
+
+ FILE *fp = fopen (out_file_name, "w");
+ pr_exp(fp, root ,2);
+ return 0;
 }
